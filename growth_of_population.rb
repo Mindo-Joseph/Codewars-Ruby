1ea3889206b1c36c0abca811a0aeeb42cea4174a
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-21 18:26:58 +0300

    solve growth of population kata

---
+++ growth_of_population.rb
@@ -0,0 +1,13 @@
+def nb_year(p0, percent, aug, p)
+  # your code
+  n = 0
+  percent *= 0.01
+
+  while p0 < p
+    p0 += (p0 * percent).to_i + aug
+    n += 1
+  end
+
+  n
+end
+print nb_year(1500, 5, 100, 5000)
