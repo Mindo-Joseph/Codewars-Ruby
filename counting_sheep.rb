180003070a7e36c3be274d4021fbf4514194c092
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-22 17:41:07 +0300

    count the number of true values in array

---
+++ counting_sheep.rb
@@ -0,0 +1,13 @@
+def countSheeps arr
+    result = arr.count(true)
+    result
+    
+end
+array1 = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ]
+array2 = []
+puts countSheeps array2
